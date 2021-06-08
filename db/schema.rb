f4dd265b11d855dc8cf179f64e4c51b8dd0291b6
seiya1911 <n.siy1911cb@gmail.com>
2021-06-08 07:42:07 +0000

    [Add]Rate機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_033405) do
+ActiveRecord::Schema.define(version: 2021_06_08_071910) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_06_07_033405) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "impressions_count"
+    t.float "rate"
   end
 
   create_table "chats", force: :cascade do |t|
