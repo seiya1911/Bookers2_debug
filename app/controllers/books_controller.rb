74db4f8a7894f012e9cce632c13e962afe56aeba
seiya1911 <n.siy1911cb@gmail.com>
2021-06-09 06:21:10 +0000

    [Add]評価順並び替えボタン

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,11 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def rate_order
+    @books = Book.all.order(rate: "DESC")
+    @book = Book.new
+  end
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   root :to => "homes#top"
   get "home/about" => "homes#about"
 
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update, :order_new] do
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update, :order_new, :order_rate] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
     get "new/order" => "books#new_order"
+    get "rate/order" => "books#rate_order"
   end
 
   resources :users, only: [:index, :show, :edit, :update] do
