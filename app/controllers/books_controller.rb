faf8758d00bf59a6ac1d71a10fd3f7949e3e399b
seiya1911 <n.siy1911cb@gmail.com>
2021-06-10 02:35:29 +0000

    [Add]categoryカラムをbookモデルに追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -61,7 +61,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :rate)
+    params.require(:book).permit(:title, :body, :rate, :category)
   end
 
   def ensure_correct_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_071910) do
+ActiveRecord::Schema.define(version: 2021_06_10_023115) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_071910) do
     t.datetime "updated_at", null: false
     t.integer "impressions_count"
     t.float "rate"
+    t.string "category"
   end
 
   create_table "chats", force: :cascade do |t|
